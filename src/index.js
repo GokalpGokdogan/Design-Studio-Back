1619521ef6cfc86f1a4098c02e9725a717a9c509
Gokalp Gokdogan <gokalp.gokdogan@gmail.com>
2025-09-24 20:54:15 +0300

    Vercel Re-org

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res) => {
-  res.status(404).json({ error: 'Not found', path: req.originalUrl });
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-  res.send('API is running. Try /api endpoints.');
+    res.send('API is running. Try /api endpoints.');
 });
 
 
 // routes
 app.use('/api', designRoutes);
 
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not found', path: req.originalUrl });
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
