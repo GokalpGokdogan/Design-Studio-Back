7e91c0d1b7a0076a3ad1f337d3ad5e483916ac31
Gokalp Gokdogan <gokalp.gokdogan@gmail.com>
2025-09-28 23:09:22 +0300

    Vercel CORS Change

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true,
 }));
 app.use(express.json());
